08499bcc532342af5f5cd9f40f2d5e5798591563
Gabriel Gamez Rojas <gabirogamez@gmail.com>
2021-10-02 19:49:48 -0500

    requerimientos sprint 3 aun no funciona en el Bot de calificación

---
+++ registro.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+let registros = [];
+
+function agregarRegistro(){
+    var usuario = document.getElementById('in_usuario').value;
+    var contrasena = document.getElementById('in_contrasena').value;
+    
+    registros.push(usuario);
+    registros.push(contrasena);
+    console.log(registros);
+    alert(registros);
+}
+
+function filtrarPorContrasena(arreglo, filtro){
+    RegistrosConstraseñas =[];
+    for (let i=0; i<arreglo.length; i++){
+        if(arreglo[i].length <= filtro){
+            RegistrosConstraseñas.push(arreglo[i]);
+        }else{
+        }
+    }
+    /*console.log(RegistrosConstraseñas);*/
+    return RegistrosConstraseñas;
+}
+
+module.exports.registros = registros;
+module.exports.filtrarPorContrasena = filtrarPorContrasena;
+module.exports.agregarRegistro = agregarRegistro;
+
